db39061efbbb4320fd94b237e48c95dc7080c15b
amazliah <amazliah@microsoft.com>
2023-11-06 14:06:27 +0200

    Added audio bus

--- scripts/audio/audio_data/audio_configuration.gd
+++ scripts/audio/audio_data/audio_configuration.gd
@@ -2,6 +2,8 @@ extends Resource
 
 class_name AudioConfiguration
 
+@export var audio_bus: AudioBusName
+
 @export_category("Sound properties")
 @export var mute : bool = false
 @export_range(0, 1) var volume : float = 1
@@ -10,3 +12,6 @@ class_name AudioConfiguration
 func apply_to(audio_stream_player):
 	audio_stream_player.volume_db = volume
 	audio_stream_player.pitch_scale = pitch
+	audio_stream_player.bus = AudioBusName.keys()[audio_bus]
+
+enum AudioBusName { Music, SFX_2D, SFX_UI }
