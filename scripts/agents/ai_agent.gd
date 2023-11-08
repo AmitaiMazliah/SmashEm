e9617ac605f8e898dec178f3f5cbf7f0e68ca154
amazliah <amazliah@microsoft.com>
2023-11-08 16:26:01 +0200

    ai wait before play

--- scripts/agents/ai_agent.gd
+++ scripts/agents/ai_agent.gd
@@ -10,16 +10,22 @@ var _played_turn : bool = false
 func _process(delta):
 	if _my_turn:
 		if !_played_turn:
-			var nearest_agent = _get_nearest_agent()
-			print(self.name, " nearest target is ", nearest_agent.name)
-			var direction = self.position.direction_to(nearest_agent.position)
-			move(direction)
 			_played_turn = true
+			randomize()
+			var random_wait_time = randf_range(min_think_time_in_secs, max_think_time_in_secs)
+			print(self.name, " will wait ", random_wait_time, " secs before playing")
+			get_tree().create_timer(random_wait_time).timeout.connect(_play_turn)
+
+func _play_turn():
+	var nearest_agent = _get_nearest_agent()
+	print(self.name, " nearest target is ", nearest_agent.name)
+	var direction = self.position.direction_to(nearest_agent.position)
+	move(direction)
 
 func _get_nearest_agent() -> Agent:
 	var min_distance
 	var nearest_agent
-	var other_agents = get_tree().get_nodes_in_group("Agents").filter(func (a): return a != self)
+	var other_agents = get_tree().get_nodes_in_group("Agents").filter(func (a): return not a.dead and a != self)
 	for other_agent in other_agents:
 		var current_distance = position.distance_to(other_agent.position)
 		if min_distance == null or current_distance < min_distance:
