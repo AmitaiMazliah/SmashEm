83e0648e16ff3e598f6f99ee807ed585db513667
amazliah <amazliah@microsoft.com>
2023-11-14 11:28:47 +0200

    Work on shop tab

---
+++ scripts/UI/shop_tab_ui.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var _items_grid: GridContainer = $MarginContainer/GridContainer
+
+@export var _item_view_prefab: PackedScene
+
+func _ready():
+	var items = _get_items_catalog()
+	for item in items:
+		var item_view = _item_view_prefab.instantiate()
+		_items_grid.add_child(item_view)
+
+func _get_items_catalog() -> Array[Equipment]:
+	var items: Array[Equipment] = []
+	var dir = DirAccess.open("res://resources/items/")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			print("File name: ", file_name)
+			var item = load("res://resources/items/" + file_name)
+			if item:
+				items.append(item)
+			file_name = dir.get_next()
+	return items
---
+++ scripts/player.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@export var nickname: String
+@export var owned_items: Array[Equipment]
+
+
