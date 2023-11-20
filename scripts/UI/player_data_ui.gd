2a277ab8e14be6a19a41560bb197d536a2fbd990
amazliah <amazliah@microsoft.com>
2023-11-20 18:16:31 +0200

    gold icon

--- scripts/UI/player_data_ui.gd
+++ scripts/UI/player_data_ui.gd
@@ -1,9 +1,11 @@
 extends Control
 
-
+@onready var _gold_label: Label = $MarginContainer/HBoxContainer/GoldPanel/Label
 
 func _ready():
 	Player.gold_changed.connect(_on_player_gold_changed)
+	_gold_label.text = str(Player.gold)
 
 func _on_player_gold_changed(player_gold: int):
+	_gold_label.text = str(player_gold)
 	print("Player now has ", player_gold, " gold")
