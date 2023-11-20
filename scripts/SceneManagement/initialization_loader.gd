ccbd3590ccc52cacfb075b376ef999525be02629
amazliah <amazliah@microsoft.com>
2023-11-20 14:34:12 +0200

    Started work with playfab

--- scripts/SceneManagement/initialization_loader.gd
+++ scripts/SceneManagement/initialization_loader.gd
@@ -4,4 +4,24 @@ extends Node
 @export var main_menu_scene: GameScene
 
 func _ready():
+	PlayFabManager.client.logged_in.connect(_on_logged_in)
+	PlayFabManager.client.api_error.connect(_on_api_error)
+	_login()
+
+func _on_logged_in(login_result: LoginResult):
+	print("Logged in successfully")
 	load_scene_event_channel.raise_event(main_menu_scene)
+
+func _on_api_error(api_error_wrapper: ApiErrorWrapper):
+	print("error")
+
+func _login():
+	print(OS.get_name())
+	match OS.get_name():
+		"Windows":
+			var combined_info_request_params = GetPlayerCombinedInfoRequestParams.new()
+			combined_info_request_params.show_all()
+			var player_profile_view_constraints = PlayerProfileViewConstraints.new()
+			combined_info_request_params.ProfileConstraints = player_profile_view_constraints
+			var tags = {}
+			PlayFabManager.client.login_with_email("a@gmail.com", "password", tags, combined_info_request_params)
