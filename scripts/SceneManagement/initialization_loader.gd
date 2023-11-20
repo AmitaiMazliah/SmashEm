8a107ea448c67e609763691224111c63bd684cb2
Amitai Mazliah <amitaimazliah@gmail.com>
2023-11-20 22:53:31 +0200

    mac login

--- scripts/SceneManagement/initialization_loader.gd
+++ scripts/SceneManagement/initialization_loader.gd
@@ -21,7 +21,7 @@ func _on_api_error(api_error_wrapper: ApiErrorWrapper):
 func _login():
 	print(OS.get_name())
 	match OS.get_name():
-		"Windows", "Web":
+		"Windows", "Web", "macOS":
 			var combined_info_request_params = GetPlayerCombinedInfoRequestParams.new()
 			combined_info_request_params.show_all()
 			var player_profile_view_constraints = PlayerProfileViewConstraints.new()
