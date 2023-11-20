b74a8c76a02eed0e3056bf2132e14a2296850fbc
Amitai Mazliah <amitaimazliah@gmail.com>
2023-11-20 23:14:32 +0200

    player trophies

--- scripts/UI/player_data_ui.gd
+++ scripts/UI/player_data_ui.gd
@@ -1,11 +1,18 @@
 extends Control
 
+@onready var _trophy_count_label: Label = $MarginContainer/HBoxContainer/TrophyPanel/Label
 @onready var _gold_label: Label = $MarginContainer/HBoxContainer/GoldPanel/Label
 
 func _ready():
+	Player.trophy_count_changed.connect(_on_trophy_count_changed)
 	Player.gold_changed.connect(_on_player_gold_changed)
-	_gold_label.text = str(Player.gold)
+	_on_trophy_count_changed(Player.trophy_count)
+	_on_player_gold_changed(Player.gold)
 
 func _on_player_gold_changed(player_gold: int):
 	_gold_label.text = str(player_gold)
 	print("Player now has ", player_gold, " gold")
+
+func _on_trophy_count_changed(trophy_count: int):
+	_trophy_count_label.text = str(trophy_count)
+	print("Player now has ", trophy_count, " trophies")
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,19 +1,30 @@
 extends Node
 
 signal gold_changed(gold: int)
+signal trophy_count_changed(trophy_count: int)
 
 var nickname: String
+
 var gold: int :
 	set (value):
 		gold = value
 		gold_changed.emit(gold)
 	get:
 		return gold
+
+var trophy_count: int :
+	set (value):
+		trophy_count = value
+		trophy_count_changed.emit(gold)
+	get:
+		return trophy_count
+
 var selected_items: Dictionary
 var owned_items: Array[PlayerEquipment]
 
 func init(items_catalog: ItemsCatalog):
 	await _get_player_currencies(items_catalog)
+	await _get_player_data()
 	print("Player initialized successfully")
 
 func _get_player_currencies(items_catalog: ItemsCatalog):
@@ -27,3 +38,8 @@ func _get_player_currencies(items_catalog: ItemsCatalog):
 		for currency in result.payload.data.Items:
 			if currency.Id == gold_playfab_id:
 				gold = currency.Amount
+
+func _get_player_data():
+	var result = await MyPlayFab.send_request({}, "/Client/GetUserData", PlayFab.AUTH_TYPE.ENTITY_TOKEN).settled
+	if result.status == Promise.Status.RESOLVED:
+		trophy_count = result.payload.data.Data.get("trophy_count", 0)
