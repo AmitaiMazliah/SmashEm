c575682b1bb4adc71ead59d76a0d3c890a697535
amazliah <amazliah@microsoft.com>
2023-11-23 17:28:36 +0200

    better UI for chests

--- scripts/UI/chest_ui.gd
+++ scripts/UI/chest_ui.gd
@@ -1,14 +1,17 @@
-extends Control
+extends Panel
 
 class_name ChestUI
 
+@onready var _button: Button = $Button
 @onready var _icon: TextureRect = $ChestIcon
 
 @onready var _locked_panel: Control = $LockedPanel
 @onready var _locked_panel_duration_label: Label = $LockedPanel/DurationLabel
 
 @onready var _opening_panel: Control = $OpeningPanel
-@onready var _opening_panel_duration_label: Label = $OpeningPanel/Duration
+@onready var _opening_panel_duration_label: Label = $OpeningPanel/DurationPanel/Duration
+
+@onready var _opened_panel: Control = $OpenedPanel
 
 var _player_chest: PlayerChest
 
@@ -22,11 +25,15 @@ func _update_ui():
 	if (_player_chest != null):
 		_icon.texture = _player_chest.chest.icon
 		if not _player_chest.started_open:
+			_button.flat = true
 			_opening_panel.hide()
+			_opened_panel.hide()
 			_locked_panel.show()
 			_locked_panel_duration_label.text = str(_player_chest.chest.duration_to_open_in_hours) + "h"
 		elif _player_chest.locked:
+			_button.flat = true
 			_locked_panel.hide()
+			_opened_panel.hide()
 			_opening_panel.show()
 			var remaining_time = _player_chest.remaining_open_time
 			var time_dict = Time.get_time_dict_from_unix_time(remaining_time)
@@ -40,4 +47,9 @@ func _update_ui():
 			else:
 				_opening_panel_duration_label.text = str(seconds) + "secs"
 		else:
-			print("Chest is opened")
+			_button.flat = false
+			_locked_panel.hide()
+			_opening_panel.hide()
+			_opened_panel.show()
+	else:
+		queue_free()
