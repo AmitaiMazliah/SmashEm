1c2bef6773d81586c1442889d250f699b2ee0543
amazliah <amazliah@microsoft.com>
2023-11-25 13:10:13 +0200

    player crystals

--- scripts/UI/player_data_ui.gd
+++ scripts/UI/player_data_ui.gd
@@ -2,17 +2,24 @@ extends Control
 
 @onready var _trophy_count_label: Label = $MarginContainer/HBoxContainer/TrophyPanel/Label
 @onready var _gold_label: Label = $MarginContainer/HBoxContainer/GoldPanel/Label
+@onready var _crystals_label: Label = $MarginContainer/HBoxContainer/CrystalsPanel/Label
 
 func _ready():
 	Player.trophy_count.value_changed.connect(_on_trophy_count_changed)
 	Player.gold.value_changed.connect(_on_player_gold_changed)
+	Player.crystals.value_changed.connect(_on_player_crystals_changed)
 	_on_trophy_count_changed(Player.trophy_count.value)
 	_on_player_gold_changed(Player.gold.value)
+	_on_player_crystals_changed(Player.crystals.value)
+
+func _on_trophy_count_changed(trophy_count: int):
+	_trophy_count_label.text = str(trophy_count)
+	print("Player now has ", trophy_count, " trophies")
 
 func _on_player_gold_changed(player_gold: int):
 	_gold_label.text = str(player_gold)
 	print("Player now has ", player_gold, " gold")
 
-func _on_trophy_count_changed(trophy_count: int):
-	_trophy_count_label.text = str(trophy_count)
-	print("Player now has ", trophy_count, " trophies")
+func _on_player_crystals_changed(player_crystals: int):
+	_crystals_label.text = str(player_crystals)
+	print("Player now has ", player_crystals, " crystals")
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,6 +3,7 @@ extends Node
 var nickname: String
 
 @export var gold: IntVariable
+@export var crystals: IntVariable
 @export var trophy_count: IntVariable
 
 var selected_items: Dictionary
@@ -21,10 +22,13 @@ func _get_player_currencies(items_catalog: ItemsCatalog):
 	var result = await MyPlayFab.send_request(dict, "/Inventory/GetInventoryItems", PlayFab.AUTH_TYPE.ENTITY_TOKEN).settled
 	if result.status == Promise.Status.RESOLVED:
 		var gold_playfab_id = items_catalog.currencies.filter(func (c): return c.name == "Gold")[0].playfab_id
+		var crystal_playfab_id = items_catalog.currencies.filter(func (c): return c.name == "Crystal")[0].playfab_id
 		
 		for currency in result.payload.data.Items:
 			if currency.Id == gold_playfab_id:
 				gold.value = currency.Amount
+			elif currency.Id == crystal_playfab_id:
+				crystals.value = currency.Amount
 
 func _get_player_data():
 	var result = await MyPlayFab.send_request({}, "/Client/GetUserData", PlayFab.AUTH_TYPE.ENTITY_TOKEN).settled
