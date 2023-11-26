c22ed7a72247852243cbd0a259a4c1497441726e
amazliah <amazliah@microsoft.com>
2023-11-26 17:59:03 +0200

    items place holders

--- scripts/agents/agent_equipment.gd
+++ scripts/agents/agent_equipment.gd
@@ -3,6 +3,24 @@ extends Node
 class_name AgentEquipment
 
 @export var current_equipment : Dictionary
+@export var _head_place_holder: Node2D
+@export var _right_hand_place_holder: Node2D
+@export var _left_hand_place_holder: Node2D
+@export var _legs_place_holder: Node2D
+
+func _ready():
+	for i in current_equipment:
+		var equipment : Equipment = current_equipment[i]
+		var equipment_instance = equipment.prefab.instantiate()
+		match equipment.slot:
+			Equipment.Slot.Head:
+				_head_place_holder.add_child(equipment_instance)
+			Equipment.Slot.RightHand:
+				_right_hand_place_holder.add_child(equipment_instance)
+			Equipment.Slot.LeftHand:
+				_left_hand_place_holder.add_child(equipment_instance)
+			Equipment.Slot.Boots:
+				_legs_place_holder.add_child(equipment_instance)
 
 func execute_all_effect_for_time(agent: Agent, effect_time: Effect.EffectTime):
 	for i in current_equipment:
--- scripts/items/equipment.gd
+++ scripts/items/equipment.gd
@@ -5,6 +5,7 @@ class_name Equipment
 @export var name: String
 @export_multiline var description: String
 @export var sprite: Texture
+@export var prefab: PackedScene
 
 @export var slot : Slot
 @export var bonus_health : int
