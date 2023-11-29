96e0c8898df115bf2f70c43d95a4da2c8edd9fe7
amazliah <amazliah@microsoft.com>
2023-11-29 14:20:21 +0200

    more work on item details ui

--- scripts/UI/core/ui_popup.gd
+++ scripts/UI/core/ui_popup.gd
@@ -4,6 +4,9 @@ class_name UIPopup
 
 var show_popup_event: ControlEventChannel = load("res://resources/events/ui/show_popup_event.tres")
 
+func _ready():
+	hide()
+
 func open():
 	show_popup_event.raise_event(self)
 
--- scripts/UI/item_details_popup.gd
+++ scripts/UI/item_details_popup.gd
@@ -2,12 +2,23 @@ extends UIPopup
 
 class_name ItemDetailsPopup
 
+const ITEM_ATTACK_BONUS_TEXT = "[font_size=28][color=black]Damage[/color]
+{damage}[/font_size]"
+const ITEM_HEALTH_BONUS_TEXT = "[font_size=28][color=black]Hitpoints[/color]
+{health}[/font_size]"
+
 @onready var _item_name_label: Label = $Panel/MarginContainer/Control/ItemName
 @onready var _item_icon: TextureRect = $Panel/MarginContainer/Control/Panel/ItemIcon
+@onready var _item_attack_bonus_label: RichTextLabel = $Panel/MarginContainer/Control/ItemStatsPanel/MarginContainer/VBoxContainer/DamageStatPanel/HBoxContainer/ItemDamageBonus
+@onready var _item_health_bonus_label: RichTextLabel = $Panel/MarginContainer/Control/ItemStatsPanel/MarginContainer/VBoxContainer/HealthStatPanel/HBoxContainer/ItemHealthBonus
+@onready var _item_description_label: RichTextLabel = $Panel/MarginContainer/Control/ItemDescriptionPanel/MarginContainer/ItemDescription
 
 func set_item(player_item: PlayerEquipment):
 	_item_name_label.text = player_item.equipment.name
 	_item_icon.texture = player_item.equipment.icon
+	_item_attack_bonus_label.text = ITEM_ATTACK_BONUS_TEXT.format({ "damage": player_item.equipment.bonus_damage })
+	_item_health_bonus_label.text = ITEM_HEALTH_BONUS_TEXT.format({ "health": player_item.equipment.bonus_health })
+	_item_description_label.text = player_item.equipment.description
 	open()
 
 func _on_close_button_pressed():
