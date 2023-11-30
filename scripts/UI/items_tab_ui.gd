857f3c6e3bd816c29daf25a1b6ab2a3a67df9560
amazliah <amazliah@microsoft.com>
2023-11-30 12:01:36 +0200

    show selected items

--- scripts/UI/items_tab_ui.gd
+++ scripts/UI/items_tab_ui.gd
@@ -1,9 +1,5 @@
 extends Control
 
-#@onready var _head_item_view: ItemSlotView = $MarginContainer/HFlowContainer/MyItemsContainer/HeadSlot
-#@onready var _right_hand_item_view: ItemSlotView = $MarginContainer/HFlowContainer/MyItemsContainer/RightHandSlot
-#@onready var _left_hand_item_view: ItemSlotView = $MarginContainer/HFlowContainer/MyItemsContainer/LeftHandSlot
-#@onready var _boots_item_view: ItemSlotView = $MarginContainer/HFlowContainer/MyItemsContainer/BootsSlot
 @onready var _my_items_container: HBoxContainer = $ScrollContainer/MarginContainer/Control/ItemsContainer/MyItemsContainer
 @onready var _owned_items_grid: GridContainer = $ScrollContainer/MarginContainer/Control/ItemsContainer/OwnedItemsGridContainer
 @onready var _item_details_popup: ItemDetailsPopup = $ItemDetailsPopup
@@ -12,6 +8,7 @@ extends Control
 @export var _items_catalog: ItemsCatalog
 
 func _ready():
+	Player.selected_items_changed.connect(_prepare_player_items_view)
 	_item_details_popup.use_item.connect(_on_use_item)
 	_prepare_player_items_view()
 	
@@ -28,19 +25,15 @@ func _create_item_view() -> ItemSlotView:
 	return item_view
 
 func _prepare_player_items_view():
-	for slot in Equipment.Slot:
-		var item = Player.selected_items.get(slot, null)
-		var item_view = _create_item_view()
-		item_view.ready.connect(item_view.set_item.bind(item))
-		_my_items_container.add_child(item_view)
-#	var head_item: Equipment = Player.selected_items.get(Equipment.Slot.Head, null)
-#	_head_item_view.set_item(head_item)
-#	var right_hand_item: Equipment = Player.selected_items.get(Equipment.Slot.RightHand, null)
-#	_right_hand_item_view.set_item(right_hand_item)
-#	var left_hand_item: Equipment = Player.selected_items.get(Equipment.Slot.LeftHand, null)
-#	_left_hand_item_view.set_item(left_hand_item)
-#	var boots_item: Equipment = Player.selected_items.get(Equipment.Slot.Boots, null)
-#	_boots_item_view.set_item(boots_item)
+	for slot_index in Equipment.Slot.values():
+		var item = Player.selected_items.get(slot_index, null)
+		if item:
+			var item_container = _my_items_container.get_child(slot_index)
+			if item_container.get_child_count() > 1:
+				item_container.get_child(1).queue_free()
+			var item_view = _create_item_view()
+			item_view.ready.connect(item_view.set_item.bind(item))
+			item_container.add_child(item_view)
 
 func _on_item_selected(item_view: ItemSlotView):
 	if item_view.item:
