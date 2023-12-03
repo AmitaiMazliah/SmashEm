c66e0b083cb789481f837d2104f2ec690528312c
amazliah <amazliah@microsoft.com>
2023-12-03 10:46:05 +0200

    3d health bar

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -1,8 +1,8 @@
 extends Control
 
-@export var health : HealthComponent
+@export var agent : MyAgent
 @onready var progress_bar : ProgressBar = $ProgressBar
 
 func _process(_delta):
-	progress_bar.max_value = health.max_health
-	progress_bar.value = health.current_health
+	progress_bar.max_value = agent.max_health
+	progress_bar.value = agent.current_health
