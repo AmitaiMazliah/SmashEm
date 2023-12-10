8d30a6b3a4389e2f8d17165e16ce73249bb47369
amazliah <amazliah@microsoft.com>
2023-12-10 16:07:44 +0200

    better movement prediction

--- scripts/agents/agent_movement_prediction.gd
+++ scripts/agents/agent_movement_prediction.gd
@@ -5,12 +5,23 @@ class_name AgentMovementPrediction
 @onready var prediction_line: Line2D = $PredictionLine
 @onready var collision_check: CharacterBody2D = $CollisionCheck
 
+@export var agent_collision_shape: CollisionShape2D
+
+var agent_collision_shape_id: RID
+
+func _ready() -> void:
+	agent_collision_shape_id = agent_collision_shape.shape.get_rid()
+
 func show_prediction(direction: Vector2, agent_speed: float, delta: float) -> void:
 	var points = calculate_movement(direction, agent_speed, delta)
 	prediction_line.points = points
 	prediction_line.show()
 	
-	var p : Vector2 = Vector2.ZERO
+	_get_position_before_collision(direction, agent_speed, delta)
+	queue_redraw()
+
+func _get_position_before_collision(direction: Vector2, agent_speed: float, delta: float) -> void:
+	var p: Vector2 = Vector2.ZERO
 	collision_check.position = p
 	var velocity = direction * agent_speed
 	var collision: KinematicCollision2D
@@ -18,7 +29,7 @@ func show_prediction(direction: Vector2, agent_speed: float, delta: float) -> vo
 	var m = true
 	while m:
 		collision = collision_check.move_and_collide(velocity * d, true)
-		while collision and collision.get_collider_shape() != $CollisionCheck/CollisionShape2D.shape and d > 0.002:
+		while collision and collision.get_collider_shape().shape.get_rid() != agent_collision_shape_id and d > 0.002:
 			d -= 0.001
 			collision = collision_check.move_and_collide(velocity * d, true)
 		if d <= 0.002:
@@ -43,3 +54,28 @@ func calculate_movement(direction: Vector2, agent_speed: float, delta: float) ->
 		if result:
 			break
 	return points
+
+func _draw():
+	var angles = [
+		{"start": 2.5, "end": 42.5},
+		{"start": 47.5, "end": 87.5},
+		{"start": 92.5, "end": 132.5},
+		{"start": 137.5, "end": 177.5},
+		{"start": 182.5, "end": 222.5},
+		{"start": 227.5, "end": 267.5},
+		{"start": 272.5, "end": 312.5},
+		{"start": 317.5, "end": 357.5}
+	]
+	for a in angles:
+		draw_circle_arc(collision_check.position, agent_collision_shape.shape.radius, a.start, a.end, Color.WHITE)
+
+func draw_circle_arc(center, radius, angle_from, angle_to, color):
+	var nb_points = 32
+	var points_arc = PackedVector2Array()
+
+	for i in range(nb_points + 1):
+		var angle_point = deg_to_rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+
+	for index_point in range(nb_points):
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color, 2)
