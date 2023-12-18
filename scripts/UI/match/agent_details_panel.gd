84a3bbe68a18eee0db5e683f7a238f39c24718f8
amazliah <amazliah@microsoft.com>
2023-12-18 11:46:22 +0200

    agent equip in panel

--- scripts/UI/match/agent_details_panel.gd
+++ scripts/UI/match/agent_details_panel.gd
@@ -12,6 +12,14 @@ const AGENT_DAMAGE_TEXT = "[font_size=24][color=black]Damage[/color]
 @onready var agent_health_label: RichTextLabel = $MarginContainer/Control/VBoxContainer/AgentStatsPanel/MarginContainer/VBoxContainer/AgentHealthPanel/HBoxContainer/AgentHealth
 @onready var agent_speed_label: RichTextLabel = $MarginContainer/Control/VBoxContainer/AgentStatsPanel/MarginContainer/VBoxContainer/AgentSpeedPanel/HBoxContainer/AgentSpeed
 @onready var agent_damage_label: RichTextLabel = $MarginContainer/Control/VBoxContainer/AgentStatsPanel/MarginContainer/VBoxContainer/AgentDamagePanel/HBoxContainer/AgentDamage
+@onready var agent_head_item_icon: TextureRect = $MarginContainer/Control/VBoxContainer/AgentItemsContainer/HeadSlot/HeadItemIcon
+@onready var default_agent_head_item_icon: TextureRect = $MarginContainer/Control/VBoxContainer/AgentItemsContainer/HeadSlot/DefaultHeadItemIcon
+@onready var agent_right_hand_item_icon: TextureRect = $MarginContainer/Control/VBoxContainer/AgentItemsContainer/RightHandSlot/RightHandItemIcon
+@onready var default_agent_right_hand_item_icon: TextureRect = $MarginContainer/Control/VBoxContainer/AgentItemsContainer/RightHandSlot/DefaultRightHandItemIcon
+@onready var agent_left_hand_item_icon: TextureRect = $MarginContainer/Control/VBoxContainer/AgentItemsContainer/LeftHandSlot/LeftHandItemIcon
+@onready var default_agent_left_hand_item_icon: TextureRect = $MarginContainer/Control/VBoxContainer/AgentItemsContainer/LeftHandSlot/DefaultLeftHandItemIcon
+@onready var agent_boots_item_icon: TextureRect = $MarginContainer/Control/VBoxContainer/AgentItemsContainer/BootsSlot/BootsItemIcon
+@onready var default_agent_boots_item_icon: TextureRect = $MarginContainer/Control/VBoxContainer/AgentItemsContainer/BootsSlot/DefaultBootsItemIcon
 @onready var statuses_container: Control = $MarginContainer/Control/VBoxContainer/StatusesContainer
 
 @export var status_details_prefab: PackedScene
@@ -24,6 +32,7 @@ func _ready() -> void:
 func set_agent(agent: Agent2D):
 	print("need to show details of ", agent.name)
 	_agent = agent
+	_set_agent_equipment_ui()
 	agent.statuses_changed.connect(_on_agent_statuses_changed)
 	_on_agent_statuses_changed(agent.statuses)
 	show()
@@ -53,3 +62,33 @@ func _on_agent_statuses_changed(statuses: Array[AgentStatus]) -> void:
 		var status_details = status_details_prefab.instantiate() as StatusDetailsUI
 		status_details.ready.connect(func (): status_details.set_agent_status(status))
 		statuses_container.add_child(status_details)
+
+func _set_agent_equipment_ui() -> void:
+	if _agent.agent_equipment.current_equipment.has(Equipment.Slot.Head):
+		default_agent_head_item_icon.hide()
+		agent_head_item_icon.show()
+		agent_head_item_icon.texture = _agent.agent_equipment.current_equipment.get(Equipment.Slot.Head).icon
+	else:
+		default_agent_head_item_icon.show()
+		agent_head_item_icon.hide()
+	if _agent.agent_equipment.current_equipment.has(Equipment.Slot.RightHand):
+		default_agent_right_hand_item_icon.hide()
+		agent_right_hand_item_icon.show()
+		agent_right_hand_item_icon.texture = _agent.agent_equipment.current_equipment.get(Equipment.Slot.RightHand).icon
+	else:
+		default_agent_right_hand_item_icon.show()
+		agent_right_hand_item_icon.hide()
+	if _agent.agent_equipment.current_equipment.has(Equipment.Slot.LeftHand):
+		default_agent_left_hand_item_icon.hide()
+		agent_left_hand_item_icon.show()
+		agent_left_hand_item_icon.texture = _agent.agent_equipment.current_equipment.get(Equipment.Slot.LeftHand).icon
+	else:
+		default_agent_left_hand_item_icon.show()
+		agent_left_hand_item_icon.hide()
+	if _agent.agent_equipment.current_equipment.has(Equipment.Slot.Boots):
+		default_agent_boots_item_icon.hide()
+		agent_boots_item_icon.show()
+		agent_boots_item_icon.texture = _agent.agent_equipment.current_equipment.get(Equipment.Slot.Boots).icon
+	else:
+		default_agent_boots_item_icon.show()
+		agent_boots_item_icon.hide()
